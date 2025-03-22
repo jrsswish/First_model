2e5d80ae5807c86540955ae7c0f166f6c011e578
jrsswish <122935245+jrsswish@users.noreply.github.com>
2025-03-21 18:50:37 -0600

    Most suicidal thoughts from Degree
    Here we find out that most suicidal thoughts are from Year 12. We still need to find why it is the case using column relations. My guess is that uncertainty for your future when graduating from high school.

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ pd.set_option('display.max_rows', 500)
 with open ('student_depression_dataset.csv', 'r') as infile:
     df = pd.read_csv(infile)
     print(df.head(5))
+
+# lets first find out which of the degree has the most suicides
+# this gives us the percentage of Yes or No from each degree.
+suicide_deg = df.groupby('Degree')['Have you ever had suicidal thoughts ?'].value_counts(normalize=True)
+suicide_deg.loc[:, 'Yes'] # locate only with Yes answers
+print(suicide_deg.sort_values(ascending=False))
