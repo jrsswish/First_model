a18e99c24bcd15c2729cdd41c05769d75a605019
jrsswish <122935245+jrsswish@users.noreply.github.com>
2025-04-02 22:15:04 -0600

    Suicidal thoughts vs age
    We can analyze from graph that age 20-30 have the most suicidal thoughts. This could be due to unsure of what to do with their futures.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.max_rows', 500)
@@ -13,3 +15,12 @@ with open ('student_depression_dataset.csv', 'r') as infile:
 suicide_deg = df.groupby('Degree')['Have you ever had suicidal thoughts ?'].value_counts(normalize=True)
 suicide_deg.loc[:, 'Yes'] # locate only with Yes answers
 print(suicide_deg.sort_values(ascending=False))
+
+# Age vs suicidal thoughts
+suicide_age = df.groupby('Age')['Have you ever had suicidal thoughts ?'].value_counts()
+suicide_age_yes = suicide_age.loc[:, 'Yes']
+plt.plot(suicide_age_yes.index, suicide_age_yes.values)
+plt.xlabel('Age')
+plt.ylabel('Said Yes to Suicidal Thoughts Count')
+plt.show()
+
