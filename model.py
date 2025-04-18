6c5e5ac69997510d1789b06e1a703740a1eb544d
jrsswish <122935245+jrsswish@users.noreply.github.com>
2025-04-17 18:50:03 -0600

    removed function for train and implemented a custom prediction function
    Removed function for train for simplicity and we also created a prediction function using degree and age as input.

--- model.py
+++ model.py
@@ -11,34 +11,44 @@ pd.set_option('display.max_rows', 500)
 
 with open ('student_depression_dataset.csv', 'r') as infile:
     df = pd.read_csv(infile)
+
+
+# sklearn expects a 2D structure so use double bracker
+X = df[['Degree', 'Age']]
+
+# put categorical feature to one hot encoding
+# drop_first removes multicollinearity
+X = pd.get_dummies(X, drop_first=True)
+
+y = df['Have you ever had suicidal thoughts ?'].map({'Yes': 1, 'No': 0})
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+model = DecisionTreeClassifier()
+
+# train the model
+model.fit(X_train, y_train)
+
+# test accuracy score for the model
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)   
+print(accuracy)
+
+
+def predict(degree, age):
+    # turn our input to dataframe
+    df = pd.DataFrame([{'Degree': degree, 'Age': age}])
+
+    # put the dataframe into the one hot encoding  
+    X = pd.get_dummies(df)
+    # makes sure that every other missing feature in this new data
+    # will have be filled with 0
+    X = X.reindex(columns=X_train.columns, fill_value=0)
+    y_pred = model.predict(X)
+
+    if y_pred[0] == 1: 
+        return 'Likely to have thoughts about suicide'
     
-# pre-cod: makes sure that df != null csv
-def train():
-        # sklearn expects a 2D structure so use double bracker
-    X = df[['Degree', 'Age']]
+    return 'Not likely to have thoughts about suicide'
 
-    # removes multicollinearity 
-    X = pd.get_dummies(X, drop_first=True)
-    y = df['Have you ever had suicidal thoughts ?'].map({'Yes': 1, 'No': 0})
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
-
-    model = DecisionTreeClassifier()
-
-    # train the model
-    model.fit(X_train, y_train)
-
-    # test accuracy score for the model
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)   
-    print(accuracy)
-""" 
- post-cod: returns a percentage prediction accuracy using two features
- degree and age to see if you are likely to think about suicide 
-"""
-
-
-
-
-
-print(train())
+print(predict('B.Pharm', 20))
